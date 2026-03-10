abf5df473b88ccd39982f00827137c6cda149152
Lautaro Casal <Laucasal27@gmail.com>
2026-03-09 21:35:46 -0300

    Add files via upload

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -1,1 +1,26 @@
+<link rel="stylesheet" href="/PP/components/footer/footer.css">
 
+<footer class="footer">
+
+    <div class="socialIcons">
+        <a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a>
+        <a href="https://www.x.com/"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://www.gmail.com/"><i class="fa-solid fa-envelope"></i></a>
+        <a href="https://www.whatsapp.com/"><i class="fa-brands fa-whatsapp"></i></a>
+    </div>
+
+    <div class="footerNav">
+        <ul>
+            <li><a href="/PP/index.php">Inicio</a></li>
+            <li><a href="/PP/pages/cartelera.php">Cartelera</a></li>
+            <li><a href="/PP/pages/contacto.php">Contacto</a></li>
+            <li><a href="https://maps.app.goo.gl/oSE14DjiAHJzYNKk7">¿Cómo llegar?</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-bottom">
+        <small>&copy; 2026 <b>Teatro Saturno</b> - Todos los Derechos Reservados.</small>
+    </div>
+
+</footer>
