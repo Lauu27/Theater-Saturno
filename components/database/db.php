c707aeea00d7f985700c3974476730423bfa1217
Lautaro Casal <Laucasal27@gmail.com>
2026-03-09 21:35:05 -0300

    Add files via upload

--- components/database/db.php
+++ components/database/db.php
@@ -1,1 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "teatro";
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
